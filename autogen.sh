0f3b2893adaf562ff42630d40ad06ab08548dad1
Giuseppe Congiu <gcongiu@icl.utk.edu>
2021-11-06 22:08:06 -0400

    make libpfm4 a git submodule

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,23 @@ echo_n() {
     printf "%s" "$*"
 }
 
+# external libraries autoreconf
+external="external/libpfm4"
+
+for i in $external; do
+    echo "autoreconf `basename $external`... "
+    cd $i && ./autogen.sh && cd - >& /dev/null
+    echo "done"
+    echo "==================================="
+    echo ""
+done
+
 # we need to create a place holder so that autoreconf
 # will not complain. Later configure fills the Makefile
 # with components Makefile files.
 touch src/components/Makefile.mk
 
+echo "autoreconf papi..."
 autoreconf -ivf
+echo "done"
+echo "==================================="
