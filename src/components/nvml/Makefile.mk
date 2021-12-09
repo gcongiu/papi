85e003be0b11ec321519810d2853ce8b611acbec
Giuseppe Congiu <gcongiu@icl.utk.edu>
2021-12-09 16:06:25 -0500

    buildsys: fix nvml test path in automake

--- src/components/nvml/Makefile.mk
+++ src/components/nvml/Makefile.mk
@@ -8,6 +8,6 @@ if BUILD_NVML
 noinst_HEADERS += src/components/nvml/linux-nvml.h
 papi_sources   += src/components/nvml/linux-nvml.c
 
-include $(top_srcdir)/src/components/tests/Makefile.mk
+include $(top_srcdir)/src/components/nvml/tests/Makefile.mk
 
 endif
