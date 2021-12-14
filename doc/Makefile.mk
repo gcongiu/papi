fca9d893ed1ff0704f43fe39ca5ec3a31f4d51e6
Giuseppe Congiu <gcongiu@icl.utk.edu>
2021-12-14 12:13:42 -0500

    buildsys: convert doxyfiles into templates for configure

---
+++ doc/Makefile.mk
@@ -0,0 +1,18 @@
+##
+## Copyright (C) by Innovative Computing Laboratory
+##     See copyright in top-level directory
+##
+
+doxygen:
+	doxygen doc/Doxyfile-html
+	doxygen doc/Doxyfile-man1
+	doxygen doc/Doxyfile-man3
+
+install-data-hook:
+	mkdir -p $(DESTDIR)$(docdir)
+	cp -r doc/doxygen/html $(DESTDIR)$(docdir)
+	cp -r doc/doxygen/man/man1 $(DESTDIR)$(docdir)
+	cp -r doc/doxygen/man/man3 $(DESTDIR)$(docdir)
+
+uninstall-data-hook:
+	rm -rf $(DESTDIR)$(docdir)
