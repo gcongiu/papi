cc31f81d6eea46f54ad1ed87cb5268677dc62bf0
Giuseppe Congiu <gcongiu@icl.utk.edu>
2022-05-13 08:45:44 -0400

    cuda: add cupti_single_thread_monitoring to tests Makefile

--- src/components/cuda/tests/hello_cupti.cu
+++ src/components/cuda/tests/hello_cupti.cu
@@ -22,62 +22,15 @@
  *  The string "Hello Cupti!" is mangled and then restored.
  */
 
-#include <cuda.h>
 #include <stdio.h>
 #include "papi.h"
 #include "papi_test.h"
+#include "common.h"
 
 #define NUM_EVENTS 1
 
 // Prototypes
 __global__ void helloCupti(char*);
-
-static int
-is_compute_capability_pre_cupti11()
-{
-    int cc_major, cc_minor;
-    CUdevice device;
-    CUresult cuErr;
-
-    cuErr = cuInit(0);
-    if (cuErr != CUDA_SUCCESS) {
-        const char *errString = NULL;
-        cuGetErrorString(cuErr, &errString);
-        fprintf(stderr, "%s:%s:%i cuInit cudaError='%s'.\n",
-                __FILE__, __func__, __LINE__, errString);
-        test_fail(__FILE__, __LINE__, "cuInit failed", 0);
-    }
-
-
-    cuErr = cuDeviceGet(&device, 0);
-    if (cuErr != CUDA_SUCCESS) {
-        const char *errString = NULL;
-        cuGetErrorString(cuErr, &errString);
-        fprintf(stderr, "%s:%s:%i cuGetDevice cudaError='%s'.\n",
-                __FILE__, __func__, __LINE__, errString);
-        test_fail(__FILE__, __LINE__, "cuGetDevice failed", 0);
-    }
-
-    cuErr = cuDeviceGetAttribute(&cc_major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, device);
-    if (cuErr != CUDA_SUCCESS) {
-        const char *errString = NULL;
-        cuGetErrorString(cuErr, &errString);
-        fprintf(stderr, "%s:%s:%i cuGetAttribute cudaError='%s'.\n",
-                __FILE__, __func__, __LINE__, errString);
-        test_fail(__FILE__, __LINE__, "cuGetAttribute failed", 0);
-    }
-
-    cuErr = cuDeviceGetAttribute(&cc_minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, device);
-    if (cuErr != CUDA_SUCCESS) {
-        const char *errString = NULL;
-        cuGetErrorString(cuErr, &errString);
-        fprintf(stderr, "%s:%s:%i cuGetAttribute cudaError='%s'.\n",
-                __FILE__, __func__, __LINE__, errString);
-        test_fail(__FILE__, __LINE__, "cuGetAttribute failed", 0);
-    }
-
-    return (cc_major < 7 || (cc_major == 7 && cc_minor == 0));
-}
 
 // Host function
 int main(int argc, char** argv)
